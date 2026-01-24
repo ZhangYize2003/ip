2ecb68a8c3bab31eff19911488da882555c7684c
ZhangYize2003 <zhangyz1601@gmail.com>
2026-01-24 23:59:13 +0800

    Rename duke and add basic greeting

---
+++ src/main/java/Sakuta.java
@@ -0,0 +1,13 @@
+public class Sakuta {
+    public static void printLine() {
+        System.out.println("-------------------------------------------------");
+    }
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Hello! I'm Sakuta.");
+        System.out.println("Is there something you want from me?");
+        printLine();
+        System.out.println("See ya. It's nice talking to you.");
+        printLine();
+    }
+}
