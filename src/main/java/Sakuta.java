e2e14d56782fb2bd05deaa4ae97a84a2eb68cf5f
ZhangYize2003 <zhangyz1601@gmail.com>
2026-01-29 18:13:53 +0800

    Add task creation and task listing

--- src/main/java/Sakuta.java
+++ src/main/java/Sakuta.java
@@ -1,29 +1,48 @@
 import java.util.Scanner;
 
 public class Sakuta {
-    public static void printLine(String line) {
+    public static void printResponse(String line) {
         System.out.println("-------------------------------------------------");
         System.out.println("Sakuta: " + line);
         System.out.println("-------------------------------------------------");
     }
 
     public static void main(String[] args) {
-        Boolean isChatting = true;
+        boolean isChatting = true;
+        Task[] tasks = new Task[100];
+        int taskNumber = 0;
 
-        printLine("Hello! I'm Sakuta.\n"
+        printResponse("Hello! I'm Sakuta.\n"
                 + "        Is there something you want from me?");
 
         while (isChatting) {
             System.out.print("> ");
             Scanner in = new Scanner(System.in);
             String line = in.nextLine();
-            if (line.equals("bye")) {
+
+            if (line.equalsIgnoreCase("bye")) {
                 isChatting = false;
+            } else if (line.equalsIgnoreCase("list")) {
+                if (taskNumber == 0) {
+                    printResponse("You have not added any task!");
+                    continue;
+                }
+
+                System.out.println("-------------------------------------------------");
+                System.out.println("__Tasks__");
+                for (int i = 0; i < taskNumber; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i].getDescription());
+                }
+                System.out.println("-------------------------------------------------");
             } else {
-                printLine(line);
+                Task task = new Task(line);
+                tasks[taskNumber] = task;
+                taskNumber++;
+
+                printResponse("I have added — " + line);
             }
         }
 
-        printLine("See ya. It's nice talking to you.");
+        printResponse("See ya. It's nice talking to you.");
     }
 }
