5a6928ffea20b7352ec8638e3cb51e1558e9b971
ZhangYize2003 <zhangyz1601@gmail.com>
2026-01-29 17:15:44 +0800

    Add echo feature

--- src/main/java/Sakuta.java
+++ src/main/java/Sakuta.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Sakuta {
-    public static void printLine() {
+    public static void printLine(String line) {
+        System.out.println("-------------------------------------------------");
+        System.out.println("Sakuta: " + line);
         System.out.println("-------------------------------------------------");
     }
+
     public static void main(String[] args) {
-        printLine();
-        System.out.println("Hello! I'm Sakuta.");
-        System.out.println("Is there something you want from me?");
-        printLine();
-        System.out.println("See ya. It's nice talking to you.");
-        printLine();
+        Boolean isChatting = true;
+
+        printLine("Hello! I'm Sakuta.\n"
+                + "        Is there something you want from me?");
+
+        while (isChatting) {
+            System.out.print("> ");
+            Scanner in = new Scanner(System.in);
+            String line = in.nextLine();
+            if (line.equals("bye")) {
+                isChatting = false;
+            } else {
+                printLine(line);
+            }
+        }
+
+        printLine("See ya. It's nice talking to you.");
     }
 }
