34ac66496ddaf2b2d03af2eb05a1c063f19e6760
ZhangYize2003 <zhangyz1601@gmail.com>
2026-02-13 01:13:30 +0800

    Add exception class

--- src/main/java/Sakuta.java
+++ src/main/java/Sakuta.java
@@ -32,95 +32,99 @@ public class Sakuta {
         greetUser();
 
         while (isChatting) {
-            System.out.print("> ");
-            Scanner in = new Scanner(System.in);
-            String line = in.nextLine().trim();
-            String[] partsBySpace = line.split("\\s+");
-            String[] partsBySlash = line.split("/");
-            String command = partsBySpace[0].toLowerCase();
+            try {
+                System.out.print("> ");
+                Scanner in = new Scanner(System.in);
+                String line = in.nextLine().trim();
+                String[] partsBySpace = line.split("\\s+");
+                String[] partsBySlash = line.split("/");
+                String command = partsBySpace[0].toLowerCase();
 
-            switch (command) {
-            case "bye":
-                isChatting = false;
-                break;
+                switch (command) {
+                case "bye":
+                    isChatting = false;
+                    break;
 
-            case "todo":
-                String toDoDesc = partsBySlash[0].substring(4).trim();
-                if (isDescriptionEmpty(toDoDesc)) {
-                    printResponse("Don't be stupid. Add a description to your task!");
+                case "todo":
+                    String toDoDesc = partsBySlash[0].substring(4).trim();
+                    if (isDescriptionEmpty(toDoDesc)) {
+                        throw new SakutaException("Don't be stupid. Add a description to your task!");
+                    }
+
+                    addTask(new Todo(toDoDesc));
+
+                    printResponse("I have added — " + toDoDesc);
+                    break;
+
+                case "deadline":
+                    String deadlineDesc = partsBySlash[0].substring(8).trim();
+                    if (isDescriptionEmpty(deadlineDesc)) {
+                        throw new SakutaException("Don't be stupid. Add a description to your task!");
+                    }
+
+                    String dueDate = partsBySlash[1].trim();
+                    addTask(new Deadline(deadlineDesc, dueDate));
+
+                    printResponse("I have added — " + deadlineDesc);
+                    break;
+
+                case "event":
+                    String eventDesc = partsBySlash[0].substring(5).trim();
+                    if (isDescriptionEmpty(eventDesc)) {
+                        throw new SakutaException("Don't be stupid. Add a description to your task!");
+                    }
+
+                    String startDate = partsBySlash[1].trim();
+                    String endDate = partsBySlash[2].trim();
+                    addTask(new Event(eventDesc, startDate, endDate));
+
+                    printResponse("I have added — " + eventDesc);
+                    break;
+
+                case "list":
+                    if (taskNumber == 0) {
+                        printResponse("You have not added any task!");
+                        break;
+                    }
+
+                    System.out.println("-----------------------------------------------------------");
+                    System.out.println("__Tasks__");
+                    for (int i = 0; i < taskNumber; i++) {
+                        System.out.println(i + 1 + ". " + tasks[i].toString());
+                    }
+                    System.out.println("\nLooks like you have " + taskNumber + " Tasks. Better start grinding!");
+                    System.out.println("-----------------------------------------------------------");
+                    break;
+
+                case "mark":
+                    int markIndex = Integer.parseInt(partsBySpace[1]) - 1;
+                    if (markIndex >= taskNumber) {
+                        printResponse("Are you trolling? This task doesn't exist!");
+                        break;
+                    }
+                    tasks[markIndex].setDone(true);
+
+                    printResponse("I have marked this task — " + tasks[markIndex].toString());
+                    break;
+
+                case "unmark":
+                    int unmarkIndex = Integer.parseInt(partsBySpace[1]) - 1;
+                    if (unmarkIndex >= taskNumber) {
+                        printResponse("Are you trolling? This task doesn't exist!");
+                        break;
+                    }
+                    tasks[unmarkIndex].setDone(false);
+
+                    printResponse("I have unmarked this task — " + tasks[unmarkIndex].toString());
+                    break;
+
+                default:
+                    // Handles any incorrect inputs
+                    printResponse("Huh? What are you even talking about?");
                     break;
                 }
-                addTask(new Todo(toDoDesc));
-
-                printResponse("I have added — " + toDoDesc);
-                break;
-
-            case "deadline":
-                String deadlineDesc = partsBySlash[0].substring(8).trim();
-                if (isDescriptionEmpty(deadlineDesc)) {
-                    printResponse("Don't be stupid. Add a description to your task!");
-                    break;
-                }
-                String dueDate = partsBySlash[1].trim();
-                addTask(new Deadline(deadlineDesc, dueDate));
-
-                printResponse("I have added — " + deadlineDesc);
-                break;
-
-            case "event":
-                String eventDesc = partsBySlash[0].substring(5).trim();
-                if (isDescriptionEmpty(eventDesc)) {
-                    printResponse("Don't be stupid. Add a description to your task!");
-                    break;
-                }
-                String startDate = partsBySlash[1].trim();
-                String endDate = partsBySlash[2].trim();
-                addTask(new Event(eventDesc, startDate, endDate));
-
-                printResponse("I have added — " + eventDesc);
-                break;
-
-            case "list":
-                if (taskNumber == 0) {
-                    printResponse("You have not added any task!");
-                    break;
-                }
-
-                System.out.println("-----------------------------------------------------------");
-                System.out.println("__Tasks__");
-                for (int i = 0; i < taskNumber; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i].toString());
-                }
-                System.out.println("\nLooks like you have " + taskNumber + " Tasks. Better start grinding!");
-                System.out.println("-----------------------------------------------------------");
-                break;
-
-            case "mark":
-                int markIndex = Integer.parseInt(partsBySpace[1]) - 1;
-                if (markIndex >= taskNumber) {
-                    printResponse("Are you trolling? This task doesn't exist!");
-                    break;
-                }
-                tasks[markIndex].setDone(true);
-
-                printResponse("I have marked this task — " + tasks[markIndex].toString());
-                break;
-
-            case "unmark":
-                int unmarkIndex = Integer.parseInt(partsBySpace[1]) - 1;
-                if (unmarkIndex >= taskNumber) {
-                    printResponse("Are you trolling? This task doesn't exist!");
-                    break;
-                }
-                tasks[unmarkIndex].setDone(false);
-
-                printResponse("I have unmarked this task — " + tasks[unmarkIndex].toString());
-                break;
-
-            default:
-                // Handles any incorrect inputs
-                printResponse("Huh? What are you even talking about?");
-                break;
+            } catch (SakutaException e) {
+                printResponse(e.getMessage());
             }
         }
 
